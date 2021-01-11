af67c2dc4ebecdb60f7125d0e1df763e4a39bfbe
Pietro <54339762+im-pokz@users.noreply.github.com>
2021-01-11 14:34:07 -0300

    Update Program.cs
    envia arquivos também

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mail;
+using System.IO;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +15,9 @@ namespace TesteEmail
         {
             Console.WriteLine("Email Spammer");
 
+            String filePath = "mantis_casado.jpeg";
+            Attachment data = new Attachment(filePath, MediaTypeNames.Application.Octet);
+
             SmtpClient client = new SmtpClient();
             client.Port = 587;
             client.Host = "smtp.gmail.com";
@@ -22,9 +27,10 @@ namespace TesteEmail
             client.UseDefaultCredentials = false;
             client.Credentials = new System.Net.NetworkCredential("awayshrimp2605@gmail.com", "Pietro2001");
 
-            MailMessage mm = new MailMessage("pietro.roque@auvo.com", "raquel.carneiro@auvo.com.br", "Parabéns pela admissão!", "Muito sucesso!");
+            MailMessage mm = new MailMessage("pietro.roque@auvo.com", "spiderpig2016@gmail.com", "Fala", "naaaaaaao stoneeee");
             mm.BodyEncoding = UTF8Encoding.UTF8;
             mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+            mm.Attachments.Add(data);
 
 
             for (int i = 0; i < 3; i++)
